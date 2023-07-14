ada356b2a2be9880ea08be08a807d8c8a88aeddf
sliu9412 <sliu9412@conestogac.on.ca>
2023-07-14 13:49:54 -0400

    init app

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PROG8470-IOS-Lab5-Multiple-Storyboards
+
 Lab5 UIKit project of PROG8470 IOS development of Conestoga College. This project has multiple storeboards with very short stories for diffrent age group children.
+
+The storyboards are navigated by preset segues.
+
+## Preview
+
+![](doc/images/2023-07-14-13-47-32-image.png)
+
+![](doc/images/2023-07-14-13-47-42-image.png)
+
+![](doc/images/2023-07-14-13-47-52-image.png)
+
+![](doc/images/2023-07-14-13-48-02-image.png)
+
+![](doc/images/2023-07-14-13-48-11-image.png)
---
+++ lab5/lab5/Information.swift
@@ -0,0 +1,29 @@
+//
+//  Information.swift
+//  lab5
+//
+//  Created by user on 2023-02-13.
+//
+
+import UIKit
+
+class Information: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
